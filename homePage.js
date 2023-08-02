bcb1b3e14d31b0552a88013d206f58a1af4bf4e4
alixfanny <alix.fanny.mail@gmail.com>
2023-08-02 17:07:14 +0200

    compraission

--- homePage.js
+++ homePage.js
@@ -14,15 +14,15 @@ insta.addEventListener("click", function(event) {
 
 function checkSize() {
     if (window.innerWidth <= 768) {
-      document.getElementById('image1').src = "./img/carousel/1-mobile.jpg";
-      document.getElementById('image2').src = "./img/carousel/2-mobile.jpg";
-      document.getElementById('image3').src = "./img/carousel/3-mobile.jpg";
-      document.getElementById('image4').src = "./img/carousel/4-mobile.jpg";
-      document.getElementById('image5').src = "./img/carousel/5-mobile.jpg";
-      document.getElementById('image6').src = "./img/carousel/6-mobile.jpg";
-      document.getElementById('image7').src = "./img/carousel/7-mobile.jpg";
-      document.getElementById('image8').src = "./img/carousel/8-mobile.jpg";
-      document.getElementById('image9').src = "./img/carousel/9-mobile.jpg";
+      document.getElementById('image1').src = "./img/1-mobile.jpg";
+      document.getElementById('image2').src = "./img/2-mobile.jpg";
+      document.getElementById('image3').src = "./img/3-mobile.jpg";
+      document.getElementById('image4').src = "./img/4-mobile.jpg";
+      document.getElementById('image5').src = "./img/5-mobile.jpg";
+      document.getElementById('image6').src = "./img/6-mobile.jpg";
+      document.getElementById('image7').src = "./img/7-mobile.jpg";
+      document.getElementById('image8').src = "./img/8-mobile.jpg";
+      document.getElementById('image9').src = "./img/9-mobile.jpg";
       
     } else {
       document.getElementById('image1').src = "./img/carousel/1.jpg";
